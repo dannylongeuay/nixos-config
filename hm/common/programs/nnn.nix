1cc2e091c1640ad228938b442a1c12b0190f29be
dannylongeuay <dannylongeuay@yahoo.com>
2024-05-10 23:16:34 -0700

    feat(nnn): :sparkles: add fuzzy finder plugin mapping

--- hm/common/programs/nnn.nix
+++ hm/common/programs/nnn.nix
@@ -3,17 +3,23 @@
 {
   programs.nnn = {
     enable = true;
-    plugins.src =
-      (pkgs.fetchFromGitHub {
-        owner = "jarun";
-        repo = "nnn";
-        rev = "v4.9";
-        sha256 = "sha256-g19uI36HyzTF2YUQKFP4DE2ZBsArGryVHhX79Y0XzhU=";
-      }) + "/plugins";
+    plugins = {
+      mappings = {
+        f = "fzcd";
+      };
+      src =
+        (pkgs.fetchFromGitHub {
+          owner = "jarun";
+          repo = "nnn";
+          rev = "v4.9";
+          sha256 = "sha256-g19uI36HyzTF2YUQKFP4DE2ZBsArGryVHhX79Y0XzhU=";
+        }) + "/plugins";
+    };
     package = pkgs.nnn.override ({ withNerdIcons = true; });
     extraPackages = with pkgs;
       [
         atool
+        fzf
         glow
         mediainfo
         sxiv
