4974dc7c45dfbd6e6a9d0e3198e0f347d0a4f28b
dannylongeuay <dannylongeuay@yahoo.com>
2024-05-20 08:29:38 -0700

    feat(kitty): :sparkles: disable audio bell and os window close confirmation

--- modules/home-manager/gui/terminal/kitty.nix
+++ modules/home-manager/gui/terminal/kitty.nix
@@ -4,6 +4,8 @@
       enable = true;
       settings = {
         shell = "fish";
+        enable_audio_bell = false;
+        confirm_os_window_close = 0;
       };
       theme = "Catppuccin-Mocha";
       font.name = "JetBrainsMono Nerd Font Mono";
